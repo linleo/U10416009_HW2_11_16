d96b7946e327825685034f74dcade865fc741d60
linleo <u10416009@go.utaipei.edu.tw>
2016-03-05 17:56:36 +0800

    Update RepeatAdditionQuiz.java
    Add some get method

--- RepeatAdditionQuiz.java
+++ RepeatAdditionQuiz.java
@@ -17,6 +17,36 @@ public class RepeatAdditionQuiz
 	//constructor
 	RepeatAdditionQuiz(){}
 	
+	//get method for first random number
+	int getRand1()
+	{
+		return rand1;
+	}
+	
+	//get method for second random number
+	int getRand2()
+	{
+		return rand2;
+	}
+	
+	//get method for first random number to multiply second random
+	int getRealAns()
+	{
+		return realAns;
+	}
+	
+	//get method for counter	 	
+	int getCounter()
+	{
+		return counter;
+	}
+	
+	//get method for answer this array list	 	
+	ArrayList<Integer> getAnswer()
+	{
+		return answer;
+	}
+	
 	//display first question
 	void displayQuestion()
 	{
